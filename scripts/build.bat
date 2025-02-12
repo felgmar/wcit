31a197110d54519f4bdfb8c47d78008caa372af0
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-02-12 19:12:27 +0000

    Fix a typo on the build script (#30)
    fix typo on the build script
    
    Signed-off-by: Felipe González Martín <158048821+felgmar@users.noreply.github.com>

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,5 +1,3 @@
 @ECHO off
 
-START "WCIT CLEANUP SCRIPT" /B scripts/cleanup.bat
-
 dotnet build "Windows Installer.sln" --nologo --self-contained --property:OutputPath=..\build\ --configuration "Debug"
