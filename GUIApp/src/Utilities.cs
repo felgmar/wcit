2b54652d144bec2d3dbd6ad68d92dce59b531b44
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-02-20 16:15:01 +0000

    GUIApp: reimplementation of the GUI program
    Taken from previous commits, I have updated and refactored this old project to make it work with the new library.

---
+++ GUIApp/src/Utilities.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Dism;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using WindowsInstallerLib.Helpers;
+
+namespace wit
+{
+    public partial class MainWindow
+    {
+        private void GetDiskLetters(object sender, EventArgs e)
+        {
+            List<string> DiskLetters = [
+                "A:\\",
+                "B:\\",
+                "C:\\",
+                "D:\\",
+                "E:\\",
+                "F:\\",
+                "G:\\",
+                "H:\\",
+                "I:\\",
+                "J:\\",
+                "K:\\",
+                "L:\\",
+                "M:\\",
+                "N:\\",
+                "O:\\",
+                "P:\\",
+                "Q:\\",
+                "R:\\",
+                "S:\\",
+                "T:\\",
+                "U:\\",
+                "V:\\",
+                "W:\\",
+                "X:\\",
+                "Y:\\",
+                "Z:\\"
+            ];
+            List<string> DiskLettersToRemove = [];
+            List<Tuple<int, string, string>> Disks = Devices.GetDisks();
+
+            try
+            {
+                foreach (string DiskLetter in DiskLetters)
+                {
+                    foreach (Tuple<int, string, string> disk in Disks)
+                    {
+                        string Letter = disk.Item3 + @"\";
+
+                        if (DiskLetter.Equals(Letter, StringComparison.Ordinal))
+                        {
+                            DiskLettersToRemove.Add(Letter);
+                        }
+                    }
+                }
+
+                foreach (string DiskLetter in DiskLettersToRemove)
+                {
+                    DiskLetters.Remove(DiskLetter);
+                }
+
+                DestinationDrive.Items.AddRange(DiskLetters.ToArray());
+                EfiDrive.Items.AddRange(DiskLetters.ToArray());
+                SourceDrive.Items.AddRange(DiskLetters.ToArray());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void GetDisksData(object sender, EventArgs e)
+        {
+            DiskList.Columns.Add("DiskNumber", "Disk");
+            DiskList.Columns.Add("Model", "Model");
+            DiskList.Columns.Add("Label", "Label");
+
+            List<Tuple<int, string, string>> disks = Devices.GetDisks();
+
+            foreach (Tuple<int, string, string> disk in disks)
+            {
+                try
+                {
+                    DiskList.Rows.Add(disk.Item1, disk.Item2, disk.Item3);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw;
+                }
+                catch (InvalidOperationException)
+                {
+                    throw;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+
+            DiskList.Sort(DiskList.Columns[0], ListSortDirection.Ascending);
+            DiskList.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            DiskNumber.Maximum = DiskList.Rows.Count - 1;
+        }
+
+        private void GetImageInfo(object sender, EventArgs e)
+        {
+            try
+            {
+                DismImageInfoCollection imageInfoCollection = Deployment.GetImageInfo(ref parameters);
+
+                WindowsEditionIndex.Minimum = 1;
+                WindowsEditionIndex.Maximum = imageInfoCollection.Count;
+
+                WindowsEditionIndex.Enabled = true;
+
+                ImageList.Columns.Add("Index", "Index");
+                ImageList.Columns.Add("Edition", "Name");
+                ImageList.Columns.Add("Version", "Version");
+
+                foreach (DismImageInfo DismImage in imageInfoCollection)
+                {
+                    ImageList.Rows.Add(DismImage.ImageIndex, DismImage.ImageName, DismImage.ProductVersion);
+                }
+            }
+            catch (DismException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                DismApi.Shutdown();
+            }
+        }
+    }
+}
---
+++ GUIApp/src/ValidateDiskLetter.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace wit
+{
+    public partial class MainWindow
+    {
+        private static void ShowMessage(string text)
+        {
+            MessageBox.Show(text, "Duplicate letters", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void ValidateDiskLetter(object? sender, EventArgs e)
+        {
+            if (DestinationDrive.Text.Length > 0)
+            {
+                if (DestinationDrive.Text == EfiDrive.Text)
+                {
+                    ShowMessage("The OS drive letter cannot be the same as the bootloader drive.");
+                    DestinationDrive.Text = null;
+                    EfiDrive.Text = null;
+                }
+                else if (DestinationDrive.Text == SourceDrive.Text)
+                {
+                    ShowMessage("The OS drive letter cannot be the same as the source drive.");
+                    DestinationDrive.Text = null;
+                    SourceDrive.Text = null;
+                }
+            }
+
+            if (EfiDrive.Text.Length > 0)
+            {
+                if (EfiDrive.Text == DestinationDrive.Text)
+                {
+                    ShowMessage("The bootloader drive letter cannot be the same as the OS drive.");
+                    EfiDrive.Text = null;
+                    DestinationDrive.Text = null;
+                }
+                else if (EfiDrive.Text == SourceDrive.Text)
+                {
+                    ShowMessage("The bootloader drive letter cannot be the same as the source drive.");
+                    EfiDrive.Text = null;
+                    SourceDrive.Text = null;
+                }
+            }
+
+            if (SourceDrive.Text.Length > 0)
+            {
+                if (SourceDrive.Text == DestinationDrive.Text)
+                {
+                    ShowMessage("The source drive letter cannot be the same as the OS drive.");
+                    SourceDrive.Text = null;
+                    DestinationDrive.Text = null;
+                }
+                else if (SourceDrive.Text == EfiDrive.Text)
+                {
+                    ShowMessage("The source drive letter cannot be the same as the bootloader drive.");
+                    SourceDrive.Text = null;
+                    EfiDrive.Text = null;
+                }
+            }
+        }
+    }
+}
