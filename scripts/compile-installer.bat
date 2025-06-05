8bcd54897a954f380b509c0068b2b1d834b17b21
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-06-05 09:01:07 +0100

    Add scripts to compile the installer

---
+++ scripts/compile-installer.bat
@@ -0,0 +1,17 @@
+@ECHO off
+
+CALL %~dp0cleanup.bat
+
+dotnet publish "Windows Installer.sln" --nologo --self-contained --property:OutputPath=%~dp0..\build\ --configuration "Release"
+
+MOVE /Y %~dp0..\build\publish %~dp0..\
+RMDIR /Q /S %~dp0..\build
+MOVE /Y %~dp0..\publish %~dp0..\build
+
+SET COMPILER="%LOCALAPPDATA%\Programs\Inno Setup 6\ISCC.exe"
+
+IF %1.==. (
+    echo ERROR: NO INNO SETUP SCRIPT WAS PROVIDED
+) ELSE (
+    CALL %COMPILER% %1
+)
