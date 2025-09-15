dd7d8e8889e2d05a9390fe9baf3bfdfee9de7731
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-09-15 19:37:43 +0100

    scripts: fix empty argument check

--- scripts/compile-installer.bat
+++ scripts/compile-installer.bat
@@ -10,7 +10,7 @@ MOVE /Y %~dp0..\publish %~dp0..\build
 
 SET COMPILER="%LOCALAPPDATA%\Programs\Inno Setup 6\ISCC.exe"
 
-IF %1.==. (
+IF %1.=="" (
     echo ERROR: NO INNO SETUP SCRIPT WAS PROVIDED
 ) ELSE (
     CALL %COMPILER% %1
